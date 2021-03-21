77b3f5e83093d583e33741d9a3ef4652b79b0f30
Peter Preinesberger <peter.preines@gmail.com>
2021-03-21 14:35:32 +0100

    21/03/21 - 2

--- include/xgame.h
+++ include/xgame.h
@@ -10,24 +10,24 @@
 #include <GL/glx.h>
 
 // Windowing and GLX Functions
-bool lw_init ();
-void lw_window (uint32_t width, uint32_t height, char* title);
-void lw_window_show ();
-void lw_init_glx ();
-void lw_window_close ();
-void lw_window_stop ();
-void lw_window_set_not_resizable ();
-bool lw_window_isopen ();
-void lw_window_update ();
-void lw_glx_swap ();
+bool xg_init ();
+void xg_window (uint32_t width, uint32_t height, char* title);
+void xg_window_show ();
+void xg_init_glx ();
+void xg_window_close ();
+void xg_window_stop ();
+void xg_window_set_not_resizable ();
+bool xg_window_isopen ();
+void xg_window_update ();
+void xg_glx_swap ();
 
 //GameDev functions
-float lw_get_ftime ();
+float xg_get_ftime ();
 
 // Keyboard Input Functions
-bool lw_keyboard_ascii (uint32_t key);
-void lw_mouse_position (int32_t* x,int32_t* y);
-void lw_set_mouse_position (int32_t x, int32_t y);
-void lw_cursor_visible (bool vis);
+bool xg_keyboard_ascii (uint32_t key);
+void xg_mouse_position (int32_t* x,int32_t* y);
+void xg_set_mouse_position (int32_t x, int32_t y);
+void xg_cursor_visible (bool vis);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,17 +1,18 @@
 #include "xgame.h"
 #include <stdio.h>
 #include <math.h>
-const uint32_t width = 1280;
-const uint32_t height = 720;
+
+const uint32_t width =  800;
+const uint32_t height = 600;
 
 int main () {
 
-	lw_init();
-	lw_window(width, height, "Window");
-	lw_window_set_not_resizable();
-	lw_init_glx();
+	xg_init();
+	xg_window(width, height, "Window");
+	xg_window_set_not_resizable();
+	xg_init_glx();
 
-	lw_window_show();
+	xg_window_show();
 
 	glClearColor(0.0f,0.0f,0.0f,1.0f);
 
@@ -27,9 +28,11 @@ int main () {
 	_dir_x = 0.0f;
 	_dir_y = 0.0f;
 	_dir_z = 0.0f;
-
 	float angle_x, angle_y;
-	lw_cursor_visible(false);
+	angle_x = 3.14159f/2;
+	angle_y = 0.0f;
+
+	xg_cursor_visible(false);
 
 	GLfloat d_verts [] = {
 		-0.5f, -0.5f, 0.0f,
@@ -46,48 +49,43 @@ int main () {
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
 
-	while(lw_window_isopen()){
+	while(xg_window_isopen()){
 
-		float frameTime = lw_get_ftime();
+		float frameTime = xg_get_ftime();
 		rotation += frameTime * 20.0f;
 
 		if(rotation > 360.0f){
 			rotation = 0.0f;
 		}
 
-		lw_window_update();
+		xg_window_update();
 
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		
-		if(lw_keyboard_ascii(27)) {
-			lw_window_stop ();
+		if(xg_keyboard_ascii(27)) {
+			xg_window_stop ();
 		}
 
-		if(lw_keyboard_ascii(0)) {
-			angle_x = 3.14f/2;
-			angle_y = 0.0f;
-		}
-
-		lw_mouse_position(&offset_x, &offset_y);
+		xg_mouse_position(&offset_x, &offset_y);
 		offset_x -= width/2;
 		offset_y -= height/2;
 		angle_x += offset_x * 0.001f;
 		angle_y += offset_y * -0.001f;
 		if(angle_y > 3.14159f/2.01f) angle_y = 3.14159f/2.01f;
 		if(angle_y < -3.14159f/2.01f) angle_y = -3.14159f/2.01f;
-		lw_set_mouse_position( width/2, height/2 );
+		xg_set_mouse_position( width/2, height/2 );
 
 		_dir_x = cos(angle_y) * cos(angle_x);
 		_dir_y = sin(angle_y);
 		_dir_z = cos(angle_y) * sin(angle_x);
 
-		if(lw_keyboard_ascii(119)){
+		if(xg_keyboard_ascii(119)){
 			_player_x += _dir_x * frameTime;
 			_player_y += _dir_y * frameTime;
 			_player_z += _dir_z * frameTime;
 		}
-		if(lw_keyboard_ascii(115)){
+		if(xg_keyboard_ascii(115)){
 			_player_x -= _dir_x * frameTime;
 			_player_y -= _dir_y * frameTime;
 			_player_z -= _dir_z * frameTime;
@@ -107,9 +105,9 @@ int main () {
 		glColorPointer(3, GL_FLOAT, 0, d_colors);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		lw_glx_swap();
+		xg_glx_swap();
 
 	}
-	lw_window_close();
+	xg_window_close();
 	return 0;
 }
